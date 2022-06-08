cc225ecf239ce86a1088b83b3b1d0c61414de067
Isabella <isabella.cunha@unesp.br>
2022-06-08 15:29:53 -0300

    inserindo novamente

--- Ex2.java
+++ Ex2.java
@@ -6,35 +6,13 @@ import javax.swing.JOptionPane;
 //Autora: Isabella Cunha
 //Exercicio 2
 
-/*Faça um programa que receba três inteiros e diga qual deles é o maior.*/
+/*Faça um programa que entre com três números e coloque em ordem crescente.*/
 
 public class Ex2 {
 	public static void main(String[] args) {
 		
-		int a,b,c, n;
 		
-		a = Integer.parseInt(JOptionPane.showInputDialog("Digite  o 1º numero:"));
-		b = Integer.parseInt(JOptionPane.showInputDialog("Digite  o 2º numero:"));
-		c = Integer.parseInt(JOptionPane.showInputDialog("Digite  o 3º numero:"));
-
-	
-			if (a > b && a > c){
-			JOptionPane.showMessageDialog(null, "O numero maior é: " + a);
-			}
-			else {
-				if(b> a && b>c) {
-					JOptionPane.showMessageDialog(null, "O numero maior é: " + b);
-				}
-				else {
-					JOptionPane.showMessageDialog(null, "O numero maior é: " + c);
-				}
-			}
 			
 }
-
-	
-	
-	
-	
 
 }
